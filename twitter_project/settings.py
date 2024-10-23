277ef9346495761b6a1359322324e3b23e451cb2
Bruno <145143785+BrunoRbt@users.noreply.github.com>
2024-10-22 23:24:23 -0300

    Add STATIC_ROOT to settings.py

--- twitter_project/settings.py
+++ twitter_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,7 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Adicione esta linha se não estiver presente
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -134,3 +138,6 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+# Adicione esta linha ao final do arquivo
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
