43912cb5983971faaa70cd24658224db97c0fa5d
Bruno <145143785+BrunoRbt@users.noreply.github.com>
2024-10-29 16:22:04 -0300

    Add home view and URL configuration

--- twitter_project/twitter_app/urls.py
+++ twitter_project/twitter_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from . import views
 from .views import register_view, login_view, tweet_view, delete_tweet_view, logout_view
 
 urlpatterns = [
+    path('', views.home, name='home'),  # Defina a view para a URL raiz
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('tweet/', tweet_view, name='tweet'),
