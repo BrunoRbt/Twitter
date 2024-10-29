2f9e12d862c31d49ccc767d41de5ae22a7f54656
Bruno <145143785+BrunoRbt@users.noreply.github.com>
2024-10-29 16:43:22 -0300

    Configure database to use Heroku environment variables

--- twitter_project/twitter_project/settings.py
+++ twitter_project/twitter_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,14 +78,7 @@ WSGI_APPLICATION = 'twitter_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'twitter_db',
-        'USER': 'postgres',
-        'PASSWORD': '369258bn',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(default='postgres://localhost:5432/twitter_db')
 }
 
 
