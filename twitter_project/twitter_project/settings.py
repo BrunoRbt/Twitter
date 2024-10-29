3541d1de84803a5f13e72dc865660912a126ca93
Bruno <145143785+BrunoRbt@users.noreply.github.com>
2024-10-29 15:06:50 -0300

    art

--- twitter_project/twitter_project/settings.py
+++ twitter_project/twitter_project/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'twitter_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgres://localhost')
+    'default': dj_database_url.config(default='postgres://postgres:369258bn@localhost:5432/DBNAME')
 }
 
 
